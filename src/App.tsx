b0aca5308b4a0c62153f5a959dee418bb4576e25
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-24 09:57:22 +0200

    Added documentation for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,23 @@ import {Switch, Route } from 'react-router-dom';
 import Map from './components/screens/Map';
 import FlightDetails from './components/screens/FlightDetails';
 
-
+/**
+ * fetchPlanLocations: -> 
+ * This function fetches Aerolaplanes data from the allFlight API.
+ * After fetching & parsing the data, the state for flightsData is then set.
+ * 
+ * 
+ * refreshData Function: ->
+ * This function calls the function which fetches flight's data above periodically
+ * This functions uses setInterval function to work as a data refreshing machanism
+ * for the App.
+ * 
+ * 
+ * toggleDetails Function: ->
+ * This function toggles the display for the Modal which renders
+ * aeroplane's data upon a click event on the aeroplane's images 
+ * or 'closewindow' button or modal's backdrop.
+ */
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [showDetails, setShowDetails] = useState(false);
