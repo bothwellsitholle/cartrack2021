bc1186651e5246801f1f002188c17314123cde81
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-24 09:47:10 +0200

    Added documentation for LoadingSpinner

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -7,6 +7,46 @@ interface Props {
   onClose: () => void;
 }
 
+/**
+ * 1)Modal Component:
+ * This creates a Portal and puts the Modal in the public/index.html
+ * as the direct child element of the body. This is a wrapper card for
+ * the content from the Modal's content component.
+ * @param onClose - This is a fuction to close the Modal component
+ * @param children - This is the ModalContent component with flight details
+ * @returns Modal (ModalOverlay + Backdrop)
+ * 
+ * 
+ * 
+ * 2)Backdrop Component:
+ * This component returns a backdrop for the Modal overlay
+ * @param onClose - receives a fuction to close the Modal component
+ * @returns backdrop for the Modaloverlay component
+ * 
+ * 
+ * 
+ * 3)ModalOverlay Component:
+ * This component is a card wrapper for the ModalContent component
+ * @param children -  ModalContent component (props.child) with flight details
+ * @returns - Card with flight details
+ * 
+ * 
+ * 
+|* 4)portalElement Element:
+ * Targets HTML Element: direct child element for the body element
+ */
+const Modal: FC<Props> = ({ onClose, children }) => {
+  const portalElement = document.getElementById('overlays') as HTMLDivElement;
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop onClose={onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
 
 const Backdrop: FC<Props> = ({ onClose }) => {
   return <div className={classes.backdrop} onClick={onClose} />;
@@ -17,20 +57,6 @@ const ModalOverlay: FC = ({ children }) => {
     <div className={classes.modal}>
       <div className={classes.content}>{children}</div>
     </div>
-  );
-};
-
-const portalElement = document.getElementById('overlays') as HTMLDivElement;
-
-const Modal: FC<Props> = ({ onClose, children }) => {
-  return (
-    <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={onClose} />, portalElement)}
-      {ReactDOM.createPortal(
-        <ModalOverlay>{children}</ModalOverlay>,
-        portalElement
-      )}
-    </Fragment>
   );
 };
 
