6b5535990966053d70cfe8024240cbae19e422b0
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-24 09:59:00 +0200

    Added documentation for index file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 
+
+/**
+ * Renders App tree
+ */
 ReactDOM.render(
   <Router>
     <App />
