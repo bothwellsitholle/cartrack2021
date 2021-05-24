9073b3714f02c482aee755457563c0e97bf95a5a
Bothwell Sithole <bothwellsitholle@gmail.com>
2021-05-24 09:45:42 +0200

    Added documentation for LoadingSpinner

--- src/components/UI/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner.tsx
@@ -1,5 +1,9 @@
 import classes from './LoadingSpinner.module.css';
 
+/**
+ * LoadingSpinner Component: ->
+ * This component displays the Loading spinner when it is executed.
+ */
 const LoadingSpinner = () => {
   return <div className={classes.spinner}></div>;
 }
